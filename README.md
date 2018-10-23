b810661199b9604d96aaef886a3315a2912a1a8a
Nate <nathanselk@gmail.com>
2018-10-23 07:44:52 -0400

    Newest COmmit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # python-pi-example
 This is an example project ot demonstrate the use of git on Ubuntu
+
+Make a change
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #f/usr/bin/python3
 
 def main(msg):
+    //Another comment
     //Adding comment on pi folder original
     //Pring a message
     print(msg)
