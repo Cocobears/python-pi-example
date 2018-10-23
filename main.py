d1c427eff4a54bc2f038bc0339a8a697d35557f6
Nate <nathanselk@gmail.com>
2018-10-23 07:53:36 -0400

    New branch

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 #f/usr/bin/python3
 
 def main(msg):
-    //Another comment
-    //Adding comment on pi folder original
-    //Pring a message
+
     print(msg)
+
+def secondary():
+    print ("New function")
 
 main("Hello People")
